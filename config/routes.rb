18c22fa1e8a8499a1539da76ead758d23d7ad5de
liz rush <liz@lizmrush.com>
2014-06-21 20:46:22 -0700

    undo dup work for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Nmconf::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#welcome'
 
-  get '/about' => 'static_pages#about'
-  get '/policies' => 'static_pages#policies'
+  get '/about' => 'pages#about'
+  get '/policies' => 'pages#policies'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
