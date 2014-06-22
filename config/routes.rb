9ae5798335028176945717705885d4f5ded9ea02
liz rush <liz@lizmrush.com>
2014-06-21 20:42:45 -0700

    begins static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Nmconf::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get '/about' => 'static_pages#about'
+  get '/policies' => 'static_pages#policies'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
